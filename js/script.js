5c467b8e84136db9205e0a673646c596aec9d817
Lailakhatun <lailakhatun7699@gmail.com>
2022-03-30 09:26:29 +0600

    w.r

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    'use stict'
+    $('.ser-slied').slick({
+      
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows:false,
+        dots:true,
+        speed:500
+      });
+
+      // testomonial slide
+
+      $('.test_slider').slick({
+      
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows:false,
+        dots:true,
+        speed:500
+      });
+});
